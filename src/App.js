988c27259429d585934ef9ac9db9cfbfb84e629c
christiangonzlez <cjavi95@gmail.com>
2019-03-13 19:00:00 -0500

    every day has one event that can be edited

--- src/App.js
+++ src/App.js
@@ -1,15 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class DayEvent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      eventDescription: "",
+      eventPopup: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.viewEvent = this.viewEvent.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      eventDescription: event.target.value,
+      eventPopup: this.state.eventPopup,
+    });
+  }
+
+  handleSubmit(event) {
+    this.setState({
+      eventDescription: this.state.eventDescription,
+      eventPopup: false,
+    });
+    event.preventDefault();
+  }
+
+  viewEvent() {
+    //Pop a modal with event description that can be edited
+    this.setState({
+      eventDescription: this.state.eventDescription,
+      eventPopup: true,
+    });
+  }
+
+  render() {
+    const eventDescription = this.state.eventDescription === "" ? "+" : this.state.eventDescription;
+
+    return(
+      <div>
+        <button className="event-button" onClick={() => this.viewEvent() }>
+          {eventDescription}
+        </button>
+        { this.state.eventPopup &&
+            <div className="event-view">
+              <form onSubmit={this.handleSubmit}>
+                <textarea value={this.state.eventDescription} onChange={this.handleChange} />
+                <input type="submit" value="Submit" />
+              </form>
+            </div>
+        }
+      </div>
+    );
+  }
+}
 
 class Day extends Component {
 
   render () {
 
     return(
-      <button className="calendar-day">
+      <div className="calendar-day">
         {this.props.value}
-      </button>
+        <DayEvent/>
+      </div>
     );
   }
 }
@@ -25,6 +82,7 @@ class Calendar extends Component {
     );
     return days;
   }
+
   render() {
     //Determine num weeks/days based on month/year
     return (
